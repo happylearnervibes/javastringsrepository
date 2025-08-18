f19ecffe99c422a952342d5916a89774a37529b7
happylearnervibes <durgaerpina.java@gmail.com>
2025-08-18 18:52:34 -0400

    commit

--- src/ArithmeticOperations.java
+++ src/ArithmeticOperations.java
@@ -1,8 +1,24 @@
+import javax.swing.event.ListDataEvent;
+
 public class ArithmeticOperations {
 
-    public int sum(int num1, int num2) {
-        return 30;//hardcoded value
-
-    }
+      //based on no of operands unary operators invloves one operand
+        // num++, 5++, num--,
+      // binary operators involves two operands
+      // num1+num2 , 10-5
+      // ternary operators involves three operands
+      // isEven? even:odd ,
+      public static void main(String[] args) {
+          int a = 10;
+          a++; // unary operator
+          int b = 10;
+          int c = a+b; // binary operator
+          boolean d = (20==c)?true:false;
+          // based on the operator, arthimetic operator do mathematical operations
+          System.out.println(a+b); // arithmetic
+          System.out.println((a+b)==30);// relational operators ==,!=,>,>=
+          System.out.println((a==10)&&(b==10));// logical operators &&,||,!
+          System.out.println("");//bitwise operators & ,|, ^,-,<<,>>
+      }
 
 }
