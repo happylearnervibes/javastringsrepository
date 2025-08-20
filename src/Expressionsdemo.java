0a5d6cbeaf4368b4e325adc8339d81b50002fefb
happylearnervibes <durgaerpina.java@gmail.com>
2025-08-19 22:49:56 -0400

    git init

--- src/Expressionsdemo.java
+++ src/Expressionsdemo.java
@@ -1,4 +1,11 @@
 public class Expressionsdemo {
-
-
+    public static void main(String[] args) {
+      // expression is ntg but stmt , it can be method invocation, stmt,.etc
+        String text = """
+                Hi How are you
+                  let me check
+                hope you are doing fine.
+                """;
+        System.out.println(text);
+    }
 }
